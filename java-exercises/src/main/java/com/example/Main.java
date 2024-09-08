1ea9a5810836f8f966a99d314f40a2157a29d641
FranciscoGL <francgutierrezlopez@gmail.com>
2024-09-08 01:06:49 -0600

    starting firt example

---
+++ java-exercises/src/main/java/com/example/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int numberOne=scanner.nextInt();
+
+        System.out.println("Enter a second number");
+        int numberTwo=scanner.nextInt();
+
+        int sum=numberOne+numberTwo;
+
+        System.out.println("El resultado de la suma es "+sum);
+
+        scanner.close();
+    }
+}
