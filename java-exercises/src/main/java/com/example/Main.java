77aebb8038414e11007f7844736c90c9b170b6b2
FranciscoGL <francgutierrezlopez@gmail.com>
2024-09-08 01:20:50 -0600

    fixing first example errors

--- java-exercises/src/main/java/com/example/Main.java
+++ java-exercises/src/main/java/com/example/Main.java
@@ -7,14 +7,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a number");
-        int numberOne=scanner.nextInt();
+        int numberOne = scanner.nextInt();
 
         System.out.println("Enter a second number");
-        int numberTwo=scanner.nextInt();
+        int numberTwo = scanner.nextInt();
 
-        int sum=numberOne+numberTwo;
+        int sum = numberOne + numberTwo;
 
-        System.out.println("El resultado de la suma es "+sum);
+        System.out.println("El resultado de la suma es " + sum);
 
         scanner.close();
     }
