9fb120f38a67bdba2d1f786d960e2859e3f0f976
FranciscoGL <francgutierrezlopez@gmail.com>
2024-09-08 20:36:14 -0600

    Bitwise operators

---
+++ java-exercises/src/main/java/com/example/Ex007_BitwiseOperators.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class Ex007_BitwiseOperators {
+    public static void main(String[] args) {
+        int a = 5; // En binario: 00000101
+        int b = 3; // En binario: 00000011
+
+        // AND
+        System.out.println("And: " + (a & b)); // 00000001 (1 en decimal)
+
+        // OR
+        System.out.println("Or: " + (a | b)); // 00000111 (7 en decimal)
+
+        // XOR
+        System.out.println("Xor: " + (a ^ b)); // 00000110 (6 en decimal)
+
+        // NOT
+        System.out.println("Not: " + ~a); // 11111010 (en complemento a dos, representa -6 en decimal)
+
+        // Desplazamiento a la izquierda
+        System.out.println("desplazamiento Izquierda: " + (a << 2)); // 00010100 (20 en decimal)
+
+        // Desplazamiento a la derecha
+        System.out.println("desplazamiento Derecha: " + (a >> 1)); // 00000010 (2 en decimal)
+    }
+}
