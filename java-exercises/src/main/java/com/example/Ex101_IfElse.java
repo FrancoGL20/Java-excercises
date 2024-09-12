c70836ebf61df632daa31ecd6584b8cea0eedd11
FranciscoGL <francgutierrezlopez@gmail.com>
2024-09-11 20:04:52 -0600

    if else

---
+++ java-exercises/src/main/java/com/example/Ex101_IfElse.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Ex101_IfElse {
+    public static void main(String[] args) {
+        int edad = 20;
+        String mensaje = (edad >= 18) ? "Eres mayor de edad" : "Eres menor de edad";
+        System.out.println(mensaje);
+    }
+}
