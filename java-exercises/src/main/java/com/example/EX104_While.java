5030df9c12c5017f4ecb7c436bc7a0c537630658
FranciscoGL <francgutierrezlopez@gmail.com>
2024-09-11 20:23:19 -0600

    while example

---
+++ java-exercises/src/main/java/com/example/EX104_While.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class EX104_While {
+    public static void main(String[] args) {
+        int contador = 1;
+        while (contador <= 5) {
+            System.out.println(contador);
+            // 1
+            // 2
+            // 3
+            // 4
+            // 5
+            contador++;
+        }
+    }
+}
